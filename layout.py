b78db64334abaf7df48859cb6ff3ea4134ad8b74
Mohammad Shahnawaz <shahnawaz919956@gmail.com>
2024-08-29 19:40:27 +0530

    Update layout.py
    changed file location for font

--- layout.py
+++ layout.py
@@ -19,7 +19,7 @@ size = width, height = 600, 400
 screen = pygame.display.set_mode(size)
 
 # Fonts
-OPEN_SANS = "assert/font/OpenSans-Regular.ttf"
+OPEN_SANS = "OpenSans-Regular.ttf"
 smallFont = pygame.font.Font(OPEN_SANS, 20)
 largeFont = pygame.font.Font(OPEN_SANS, 40)
 
